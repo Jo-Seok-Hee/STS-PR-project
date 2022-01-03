c18b5c2b0a38f86924f10f9b5de0afd97cdcc65b
q <whtjrgml17@naver.com>
2022-01-03 21:49:24 +0900

    01/03 연습문제 학원끝

--- src/main/java/com/sucky/pr/p04/seller/bo/SellerBO.java
+++ src/main/java/com/sucky/pr/p04/seller/bo/SellerBO.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sucky.pr.p04.seller.dao.SellerDAO;
+import com.sucky.pr.p04.seller.model.Seller;
 
 @Service
 public class SellerBO {
@@ -11,7 +12,15 @@ public class SellerBO {
 	@Autowired
 	private SellerDAO sellerDAO;
 	
-	public int addSeller(String nickname, String url, String temperature) {
+	public int addSeller(String nickname, String url, double temperature) {
 		return sellerDAO.insertSeller(nickname, url, temperature);
+	}
+	
+	public Seller getSeller(int id) {
+		return sellerDAO.selectSeller(id);
+	}
+	
+	public Seller getLastSeller() {
+		return sellerDAO.selectLastSeller();
 	}
 }
--- src/main/java/com/sucky/pr/p04/seller/sellerController.java
+++ src/main/java/com/sucky/pr/p04/seller/sellerController.java
@@ -2,10 +2,13 @@ package com.sucky.pr.p04.seller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sucky.pr.p04.seller.bo.SellerBO;
+import com.sucky.pr.p04.seller.model.Seller;
 
 @Controller
 public class sellerController {
@@ -13,18 +16,44 @@ public class sellerController {
 	@Autowired
 	private SellerBO sellerBO;
 	
-	
+	@ResponseBody
 	@RequestMapping("/p04/pr01/view")
 	public String firstView() {
 		
 		return "/p04/sellerView";
 	}
-	
+	@ResponseBody
 	@RequestMapping("/p04/pr01/return")
-	public String pr01() {
+	public String pr01(
+			@RequestParam("name")String name
+			,@RequestParam("url")String url
+			,@RequestParam("temperature")double temperature
+			) {
 		
+		int count = sellerBO.addSeller(name, url, temperature);
 		
+		return "성공 : "+count;
 	}
+	
+	
+	
+	@RequestMapping("/p04/pr02")
+	public String pr02(Model model,@RequestParam(value="id", required=false
+		
+			)Integer id) {
+		
+		if(id==null) {
+			Seller seller = sellerBO.getLastSeller();
+			model.addAttribute("result", seller);
+		} else {
+			Seller seller = sellerBO.getSeller(id);
+			model.addAttribute("result", seller);
+		}
+		model.addAttribute("info", "판매자 정보");
+		
+		return "/p04/sellerInfo";
+	}
+	
 	
 	
 }
